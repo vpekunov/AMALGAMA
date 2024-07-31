689fe276f3b4a6ee661d7d09eb03fdc3539871f5
Владимир Пекунов <pekunov@mail.ru>
2024-07-31 18:21:42 +0300

    Lost function recovered

--- run_fpc.bat
+++ run_fpc.bat
@@ -1,3 +1,3 @@
 @echo off
-c:\lazarus\fpc\3.2.2\bin\i386-win32\fpc -B -O3 -WC -Mobjfpc -FcUTF-8 %1.pas > %2
-@cls
+c:\lazarus64\fpc\3.2.2\bin\x86_64-win64\fpc -B -O3 -WC -Mobjfpc -FcUTF-8 %1.pas > %2
+1@cls
